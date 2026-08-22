3160d681d989a7c72498a1c8bed96b870e53c112
Alex Rodrigues da Silva <alexrsilva@gmail.com>
2026-08-22 19:10:15 -0300

    Changes to run the app fine

--- salao-api/src/main/java/com/salao/api/Application.java
+++ salao-api/src/main/java/com/salao/api/Application.java
@@ -2,10 +2,24 @@ package com.salao.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {
         "com.salao.api",
+        "com.salao.security",
+        "com.salao.cliente",
+        "com.salao.agendamento",
+        "com.salao.financeiro"
+})
+@EnableJpaRepositories(basePackages = {
+        "com.salao.security",
+        "com.salao.cliente",
+        "com.salao.agendamento",
+        "com.salao.financeiro"
+})
+@EntityScan(basePackages = {
         "com.salao.security",
         "com.salao.cliente",
         "com.salao.agendamento",
